e4d69a9c2dc565f40cc6f7735fd4a0767c2841a2
Deyber <clandc81@gmail.com>
2022-08-18 23:35:45 -0500

    agregando ID usuario

--- procesos/procesos.c
+++ procesos/procesos.c
@@ -6,14 +6,16 @@ int main()
  
    pid_t id_proceso_padre;
    pid_t id_proceso_hijo;
-   puts("\n==== ESTE PROGRAMA MUESTRA EL IDENTIFICADOR DE UN PROCESO PADRE E HIJO====\n");
+   uid_t id_procesos_usuario;
+   puts("\n==== ESTE PROGRAMA MUESTRA EL IDENTIFICADOR DE UN PROCESO PADRE , HIJO Y USUARIO====\n");
  
    id_proceso_padre = getppid();
    id_proceso_hijo = getpid();
+   id_procesos_usuario = getuid();
  
    printf("Identificador del proceso padre : %d\n", id_proceso_padre);
    printf("Identificador del proceso hijo: %d\n", id_proceso_hijo);
- 
+   printf("Identificador del usuario: %d\n", id_procesos_usuario);
    return 0;
  
 }
