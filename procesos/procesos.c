e84181b4345c6641550890204573379151e6f6e8
Deyber <clandc81@gmail.com>
2022-08-19 11:21:37 -0500

    aumentndo nomnbre

--- procesos/procesos.c
+++ procesos/procesos.c
@@ -6,16 +6,18 @@ int main()
  
    pid_t id_proceso_padre;
    pid_t id_proceso_hijo;
-   uid_t id_procesos_usuario;
-   puts("\n==== ESTE PROGRAMA MUESTRA EL IDENTIFICADOR DE UN PROCESO PADRE , HIJO Y USUARIO====\n");
+   uid_t uid_usuario;
+   puts("\n==== ESTE PROGRAMA MUESTRA EL IDENTIFICADOR DE UN PROCESO PADRE, HIJO , ID USUARIO Y NOMBRE DEL USUARIO====\n");
  
    id_proceso_padre = getppid();
    id_proceso_hijo = getpid();
-   id_procesos_usuario = getuid();
- 
+   uid_usuario = getuid();
+  
    printf("Identificador del proceso padre : %d\n", id_proceso_padre);
    printf("Identificador del proceso hijo: %d\n", id_proceso_hijo);
-   printf("Identificador del usuario: %d\n", id_procesos_usuario);
+   printf("UID del proceso padre: %s\n", getlogin());
+   printf("UID del proceso padre: %d\n", uid_usuario);
+   
    return 0;
  
 }
